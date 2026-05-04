d020934d0b0a821bd24fda613d6c3151766a8217
leojussleo-tester <leojussleo@gmail.com>
2026-05-05 01:15:12 +0700

    Configure Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/auction-credit-platform-demo/',
   plugins: [react()],
 });
